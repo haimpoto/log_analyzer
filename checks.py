dabb56f261fe913fc95b63bd5b84993561d0166b
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-14 14:31:38 +0300

    checks update: chaining generators added

--- checks.py
+++ checks.py
@@ -1,9 +1,16 @@
 from typing import Generator
-import reader
 
 
-def extract_external_IP(logs: Generator[list[str]]) -> list[str]:
-    external_IP_list = [lst[2] for lst in logs if not (lst[2].startswith("10") or lst[2].startswith("192.168"))]
+def extract_external_IP(logs: Generator[list[str]]) -> Generator[str]:
+    external_IP_list = (lst[1] for lst in logs if not (lst[1].startswith("10.") or lst[1].startswith("192.168")))
     return external_IP_list
 
-print(extract_external_IP(reader.get_lists(reader.the_path)))
+
+def extract_external_port(logs: Generator[list[str]]) -> Generator[list[str]]:
+    external_port_list = (lst for lst in logs if (lst[3] == "22" or lst[3] == "23" or lst[3] == "3389"))
+    return external_port_list
+
+
+def extract_large_port(logs: Generator[list[str]]) -> Generator[list[str]]:
+    external_large_list = (lst for lst in logs if int(lst[5]) >= 5000)
+    return external_large_list
