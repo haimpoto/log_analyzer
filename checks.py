4344eb6f6b257339bcdc7e72b722e812f9968948
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-14 12:08:26 +0300

    checks update

--- checks.py
+++ checks.py
@@ -3,5 +3,7 @@ import reader
 
 
 def extract_external_IP(logs: Generator[list[str]]) -> list[str]:
-    external_IP_list = [lst[2] for lst in logs if not (lst[2].startswith("10") or lst[2].startswith("192.168")
+    external_IP_list = [lst[2] for lst in logs if not (lst[2].startswith("10") or lst[2].startswith("192.168"))]
     return external_IP_list
+
+print(extract_external_IP(reader.get_lists(reader.the_path)))
