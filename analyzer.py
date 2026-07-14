3ef7232cddc1d2c82f4880a3aad8541a6b039f8e
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-14 19:08:55 +0300

    all lsts raplace to log

--- analyzer.py
+++ analyzer.py
@@ -5,26 +5,26 @@ import reader
 
 # stage_1 functions
 def extract_external_IP(logs: Generator[list[str]]) -> Generator[str]:
-    external_IP_list = (lst[1] for lst in logs if not is_external_IP(lst))
+    external_IP_list = (log[1] for log in logs if not is_external_IP(log))
     return external_IP_list
 
 
 def filter_by_external_port(logs: Generator[list[str]]) -> Generator[list[str]]:
-    external_port_list = (lst for lst in logs if is_external_port(lst))
+    external_port_list = (log for log in logs if is_external_port(log))
     return external_port_list
 
 
 def filter_by_large_port(logs: Generator[list[str]]) -> Generator[list[str]]:
-    external_large_list = (lst for lst in logs if is_large_port(lst))
+    external_large_list = (log for log in logs if is_large_port(log))
     return external_large_list
 
 
 def append_tag(logs: Generator[list[str]]) -> Generator[list[str]]:
-    for lst in logs:
-        if is_large_port(lst):
-            yield lst + ["LARGE"]
+    for log in logs:
+        if is_large_port(log):
+            yield log + ["LARGE"]
         else:
-            yield lst + ["NORMAL"]
+            yield log + ["NORMAL"]
 
 
 # stage_2 functions
@@ -35,6 +35,3 @@ def get_source_IP_dictionary(logs: Generator[list[str]]) -> dict[str, int]:
             dictionary[log[1]] = 0
         dictionary[log[1]] += 1
     return dictionary
-
-print(get_source_IP_dictionary(reader.get_lists(reader.the_path)))
-
