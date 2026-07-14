73923b6c6d2f1accfcbecea92c0417ecd19010b0
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-14 16:57:55 +0300

    analyzer added end checks is tidy

---
+++ analyzer.py
@@ -0,0 +1,25 @@
+from typing import Generator
+from checks import *
+
+
+def extract_external_IP(logs: Generator[list[str]]) -> Generator[str]:
+    external_IP_list = (lst[1] for lst in logs if is_external_IP(lst))
+    return external_IP_list
+
+
+def extract_external_port(logs: Generator[list[str]]) -> Generator[list[str]]:
+    external_port_list = (lst for lst in logs if is_external_port(lst))
+    return external_port_list
+
+
+def extract_large_port(logs: Generator[list[str]]) -> Generator[list[str]]:
+    external_large_list = (lst for lst in logs if is_large_port(lst))
+    return external_large_list
+
+
+def append_tag(logs: Generator[list[str]]) -> Generator[list[str]]:
+    for lst in logs:
+        if is_large_port(lst):
+            yield lst + ["LARGE"]
+        else:
+            yield lst + ["NORMAL"]
--- checks.py
+++ checks.py
@@ -1,24 +1,13 @@
-from typing import Generator
+from config import *
 
 
-def extract_external_IP(logs: Generator[list[str]]) -> Generator[str]:
-    external_IP_list = (lst[1] for lst in logs if not (lst[1].startswith("10.") or lst[1].startswith("192.168")))
-    return external_IP_list
+def is_external_IP(lst: list[str]) -> bool:
+    return lst[1].startswith(EXTERNAL_IP)
 
 
-def extract_external_port(logs: Generator[list[str]]) -> Generator[list[str]]:
-    external_port_list = (lst for lst in logs if (lst[3] == "22" or lst[3] == "23" or lst[3] == "3389"))
-    return external_port_list
+def is_external_port(lst: list[str]) -> bool:
+    return lst[3] in EXTERNAL_PORT
 
 
-def extract_large_port(logs: Generator[list[str]]) -> Generator[list[str]]:
-    external_large_list = (lst for lst in logs if int(lst[5]) >= 5000)
-    return external_large_list
-
-
-def append_tag(logs: Generator[list[str]]) -> Generator[list[str]]:
-    for lst in logs:
-        if lst[5] >= 5000:
-            yield lst + ["LARGE"]
-        else:
-            yield lst + ["NORMAL"]
+def is_large_port(lst: list[str]) -> bool:
+    return int(lst[5]) >= MAX_NORMAL_SIZE
