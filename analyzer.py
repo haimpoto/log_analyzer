6faf9fc9b0b5d73cd274e2300b9a4369bba76a76
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-14 20:57:24 +0300

    stage_2 is olmost completed

--- analyzer.py
+++ analyzer.py
@@ -3,23 +3,23 @@ from checks import *
 
 
 def extract_external_IP(logs: Generator[list[str]]) -> Generator[str]:
-    external_IP_list = (log[1] for log in logs if not is_external_IP(log))
+    external_IP_list = (log[1] for log in logs if is_external_IP(log))
     return external_IP_list
 
 
 def filter_by_external_port(logs: Generator[list[str]]) -> Generator[list[str]]:
-    external_port_list = (log for log in logs if is_external_port(log))
+    external_port_list = (log for log in logs if is_sensitive_port(log))
     return external_port_list
 
 
 def filter_by_large_port(logs: Generator[list[str]]) -> Generator[list[str]]:
-    external_large_list = (log for log in logs if is_large_port(log))
+    external_large_list = (log for log in logs if is_large_packet(log))
     return external_large_list
 
 
 def append_tag(logs: Generator[list[str]]) -> Generator[list[str]]:
     for log in logs:
-        if is_large_port(log):
+        if is_large_packet(log):
             yield log + ["LARGE"]
         else:
             yield log + ["NORMAL"]
--- reporter.py
+++ reporter.py
@@ -1,4 +1,5 @@
 from typing import Generator
+from checks import *
 import reader
 
 
@@ -15,4 +16,34 @@ def map_port_to_protocol(logs: Generator[list[str]]) -> dict[str, str]:
     return {log[3]: log[4] for log in logs}
 
 
-print(map_port_to_protocol(reader.get_lists(reader.the_path)))
+def analyze_ip_activity(logs: Generator[list[str]]) -> dict[str, list[str]]:
+    dictionary = {}
+    for log in logs:
+        lst = []
+        if is_external_IP(log):
+            lst.append("EXTERNAL_IP")
+        if is_sensitive_port(log):
+            lst.append("SENSITIVE_PORT")
+        if is_large_packet(log):
+            lst.append("LARGE_PACKET")
+        if is_night_activity(log):
+            lst.append("NIGHT_ACTIVITY")
+        if not lst:
+            continue
+        if log[1] not in dictionary:
+            dictionary[log[1]] = []
+        for tag in lst:
+            if tag not in dictionary[log[1]]:
+                dictionary[log[1]].append(tag)
+    return dictionary
+
+
+print(analyze_ip_activity(reader.get_lists(reader.the_path)))
+results = analyze_ip_activity(reader.get_lists(reader.the_path))
+
+# כתיבה לקובץ במקום הדפסה לטרמינל
+with open("output.txt", "w") as f:
+    for ip, tags in results.items():
+        f.write(f"{ip}: {tags}\n")
+
+print("Finished! Check output.txt for the full results.")
