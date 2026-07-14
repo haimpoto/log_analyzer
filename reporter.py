72b2472bdacc9c62a18ea295fd290f83187c01f3
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-14 19:39:19 +0300

    def map_port_to_protocol added

--- reporter.py
+++ reporter.py
@@ -1,10 +1,18 @@
 from typing import Generator
+import reader
 
 
-def get_source_IP_dictionary(logs: Generator[list[str]]) -> dict[str, int]:
+def get_source_ip(logs: Generator[list[str]]) -> dict[str, int]:
     dictionary = {}
     for log in logs:
         if log[1] not in dictionary:
             dictionary[log[1]] = 0
         dictionary[log[1]] += 1
     return dictionary
+
+
+def map_port_to_protocol(logs: Generator[list[str]]) -> dict[str, str]:
+    return {log[3]: log[4] for log in logs}
+
+
+print(map_port_to_protocol(reader.get_lists(reader.the_path)))
