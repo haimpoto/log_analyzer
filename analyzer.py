59d6f300dd7417773986f5f8bf2bbf74e43eb82f
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-17 15:34:00 +0300

    some tidy in analizer

--- analyzer.py
+++ analyzer.py
@@ -2,22 +2,22 @@ from typing import Generator
 from checks import *
 
 
-def extract_external_IP(logs: Generator[list[str]]) -> Generator[str]:
+def filter_external_IP(logs: Generator[list[str]]) -> Generator[str]:
     external_IP_list = (log[1] for log in logs if is_external_IP(log))
     return external_IP_list
 
 
-def filter_by_external_port(logs: Generator[list[str]]) -> Generator[list[str]]:
-    external_port_list = (log for log in logs if is_sensitive_port(log))
+def filter_sensitive_port(logs: Generator[list[str]]) -> Generator[str]:
+    external_port_list = (log[1] for log in logs if is_sensitive_port(log))
     return external_port_list
 
 
-def filter_by_large_port(logs: Generator[list[str]]) -> Generator[list[str]]:
+def filter_large_packets(logs: Generator[list[str]]) -> Generator[list[str]]:
     external_large_list = (log for log in logs if is_large_packet(log))
     return external_large_list
 
 
-def append_tag(logs: Generator[list[str]]) -> Generator[list[str]]:
+def tag_traffic_size(logs: Generator[list[str]]) -> Generator[list[str]]:
     for log in logs:
         if is_large_packet(log):
             yield log + ["LARGE"]
