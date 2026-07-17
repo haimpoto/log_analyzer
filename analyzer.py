1f65e4a6b390e067e3a689aeafac82e290299268
חיים פוטולסקי <haimpoto@gmail.com>
2026-07-17 16:04:00 +0300

    analizer tidy and almost complete

--- analyzer.py
+++ analyzer.py
@@ -7,8 +7,8 @@ def filter_external_IP(logs: Generator[list[str]]) -> Generator[str]:
     return external_IP_list
 
 
-def filter_sensitive_port(logs: Generator[list[str]]) -> Generator[str]:
-    external_port_list = (log[1] for log in logs if is_sensitive_port(log))
+def filter_sensitive_port(logs: Generator[list[str]]) -> Generator[list[str]]:
+    external_port_list = (log for log in logs if is_sensitive_port(log))
     return external_port_list
 
 
@@ -24,7 +24,7 @@ def tag_traffic_size(logs: Generator[list[str]]) -> Generator[list[str]]:
         else:
             yield log + ["NORMAL"]
 
-def get_source_ip(logs: Generator[list[str]]) -> dict[str, int]:
+def count_requests_per_ip(logs: Generator[list[str]]) -> dict[str, int]:
     dictionary = {}
     for log in logs:
         if log[1] not in dictionary:
@@ -35,7 +35,7 @@ def get_source_ip(logs: Generator[list[str]]) -> dict[str, int]:
 def map_port_to_protocol(logs: Generator[list[str]]) -> dict[str, str]:
     return {log[3]: log[4] for log in logs}
 
-def analyze_ip_activity(logs: Generator[list[str]]) -> dict[str, list[str]]:
+def map_ips_to_suspicions(logs: Generator[list[str]]) -> dict[str, list[str]]:
     dictionary = {}
     for log in logs:
         lst = []
@@ -56,35 +56,32 @@ def analyze_ip_activity(logs: Generator[list[str]]) -> dict[str, list[str]]:
                 dictionary[log[1]].append(tag)
     return dictionary
 
-def map_external_ip(dictionary: dict[str, list[str]]) -> dict[str, list[str]]:
-    new_dictionary = {}
-    for key, val in dictionary.items():
-        if len(val) >= 2 and key not in new_dictionary:
-            new_dictionary[key] = val
-    return new_dictionary
+def filter_multiple_suspicions(dictionary: dict[str, list[str]]) -> dict[str, list[str]]:
+    return {key: val for key, val in dictionary.items() if len(val) >= 2}
 
 
-def get_timestamps(logs: Generator[list[str]]) -> list[int]:
-    return list(map(lambda log: int(log[11:13]), logs))
+def extract_hours_with_maps(logs: Generator[list[str]]) -> list[int]:
+    return list(map(lambda log: int(log[0][11:13]), logs))
 
 
-def get_bytes(logs: Generator[list[str]]) -> list[int]:
-    return list(map(lambda log: int(log.split()[-1]) / 1024, logs))
+def convert_size_to_kb_with_map(logs: Generator[list[str]]) -> list[int]:
+    return list(map(lambda log: int(log[5]) / 1024, logs))
 
 
-def filter_by_external_port_with_lambda(logs: Generator[list[str]]) -> filter:
+def filter_sensitive_ports_with_lambda(logs: Generator[list[str]]) -> filter:
     return filter(lambda log: is_sensitive_port(log), logs)
 
 
-def filter_by_time_with_lambda(logs: Generator[list[str]]) -> filter:
+def filter_night_activity_with_lambda(logs: Generator[list[str]]) -> filter:
     return filter(lambda log: is_night_activity(log), logs)
 
 
-suspicion_checks = {
-    "EXTERNAL_IP": lambda row: row [1].startwish(INTERNAL_IP),
-    "SENSITIVE_PORT": lambda row: row[3].strip in EXTERNAL_PORTS,
-    "LARGE_PACKET": lambda row: int(row[5]) >= MAX_NORMAL_SIZE,
-    "NIGHT_ACTIVITY": lambda row: int(EXIT_TIME[:2]) <= int(row[0][11:13]) < int(ENTER_TIME[:2])
+def get_suspicion_checks_dict() -> dict:
+    return {
+        "EXTERNAL_IP": lambda row: not row[1].startswith(INTERNAL_IP),
+        "SENSITIVE_PORT": lambda row: row[3].strip() in EXTERNAL_PORTS,
+        "LARGE_PACKET": lambda row: int(row[5]) >= MAX_NORMAL_SIZE,
+        "NIGHT_ACTIVITY": lambda row: int(EXIT_TIME[:2]) <= int(row[0][11:13]) < int(ENTER_TIME[:2])
     }
 
 
